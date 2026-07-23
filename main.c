e9f0f4f71b5f34125df0251b06a3a59a1b0b6044
Lydia Schawer <lschawer@c3r9p7.42vienna.com>
2026-07-23 11:34:43 +0200

    minor bug fixes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lschawer <lschawer@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 16:07:45 by lschawer          #+#    #+#             */
-/*   Updated: 2026/07/23 10:52:01 by lschawer         ###   ########.fr       */
+/*   Updated: 2026/07/23 11:31:36 by lschawer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ int	main(int argc, char **argv)
 	t_config		cfg;
 	char			**tokens;
 
+	ft_printf("argc = %d\n", argc);
+	for (int i = 0; i < argc; i++)
+    	ft_printf("argv[%d] = '%s'\n", i, argv[i]);
 	// initialize stack a & b, point to NULL
 	a = NULL;
 	b = NULL;
@@ -47,7 +50,7 @@ int	main(int argc, char **argv)
 	}
 	assign_index(a);
 	// free_num_array(tokens);
-	// print_stack(a);
+	print_stack(a);
 	// SORTING
 	// check: stack a already sorted?
 	//		if not:  SORTING ALGORITHM MAGIC  happens here
@@ -60,6 +63,8 @@ int	main(int argc, char **argv)
 // chmod +x test.sh
 // ./test.sh
 
+// ./push_swap --medium --bench 7211 2641 9992 1374 3166 6081 2041 7995 8378 777
+
 // ./push_swap 1 2 3 6 5 4 7 8 9 13 12 11 14 16 15 18 17 19 50 40 60 20 80 90 10
 // ./push_swap --bench 1 2 3 6 5 4 7 8 9 13 12 11 14 16 15 18 17 19 50 40 60 20 80 90 10
-// ./push_swap 1 2 3 6 5 4 7 8 9 13 12 11 14 16 15 18 17 19 50 40 60 20 80 90 10
+// ./push_swap --bench 100 200 600 500 400 800 900 1200 1100 170000 1400 800000 1600 1500 1800 1700 1900 5000 4000 6000 2000 8000 9000 1000 10000 20000 30000 700 60000 50000 40000 1300 400000 70000 300 80000 90000 130000 120000 110000 140000 150000 180000 190000 500000 600000 200000 900000 100000 160000
